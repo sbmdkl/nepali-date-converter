2c18c4c3e2e5cae7f06a825f9ed0b568a537e290
Shubham Dhakal <sbmdkl@gmail.com>
2024-06-21 20:56:26 +0545

    fixed ,

--- examples/index.ts
+++ examples/index.ts
@@ -1,6 +1,6 @@
 const { adToBs, bsToAd, calculateAge } = require('../dist/@sbmdkl/nepali-date-converter.umd.js');
 try {
-	console.log(adToBs('2016-02-15'));
+	console.log(adToBs('2024-06-14'));
 	console.log(calculateAge('2070-10-15'));
 } catch (e) {
 	console.log(e.message);
